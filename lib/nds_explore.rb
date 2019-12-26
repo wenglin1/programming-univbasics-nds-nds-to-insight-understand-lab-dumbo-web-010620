689b480a8bbe9613a11cbd70e1852468d827ec08
weng lin <wenglin@buffalo.edu>
2019-12-26 23:24:20 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,6 +10,7 @@ end
 pretty_print_nds(directors_database)
 
 def print_first_directors_movie_titles
+  movies_arr = directors_database[0][:movies]
   movies_arr_index = 0
   while  movies_arr_index < movies_arr.count do
     puts movies_arr[movies_arr_index][:title]
